f2144f0b4626b9689bc0fed39015d07f806e3510
gyhaan <g.yhaan@alustudent.com>
2024-04-06 22:51:47 +0200

    Added params

--- src/UI/Home UI/CoinRow.jsx
+++ src/UI/Home UI/CoinRow.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { AreaChart, Area, ResponsiveContainer, YAxis, Tooltip } from "recharts";
 
 import ProgressBar from "./ProgressBar";
@@ -10,6 +11,7 @@ function CoinRow({ coin }) {
   console.log(coin);
   const {
     name,
+    id,
     image,
     current_price,
     price_change_percentage_1h_in_currency,
@@ -35,122 +37,140 @@ function CoinRow({ coin }) {
     });
 
   return (
-    <div className="bg-custom-dark-navy text-[#fff] flex justify-around items-center h-20 text-[14px] p-5 rounded-lg mb-4">
-      <span className="max-w-[3%] w-full mr-2 px-1">{market_cap_rank}</span>
-      <div className="max-w-[15%] w-full flex px-1 ml-[-12px]">
-        <img src={image} alt="" className="w-[20px] h-auto" />
-        <span className="px-3">{name}</span>
-      </div>
-      <span className="max-w-[8%] w-full px-1 ml-[-12px]">
-        $ {formatNumberWithK(current_price)}
-      </span>
-      <div className=" max-w-[7%] w-full flex ml-[-12px] pr-2">
-        {price_change_percentage_1h_in_currency > 0 && (
-          <img src="/chevron-up.svg" alt="" />
-        )}
-        {price_change_percentage_1h_in_currency < 0 && (
-          <img src="/chevron-down.svg" alt="" />
-        )}
+    <Link to={`coin/${id}`}>
+      <div className="bg-custom-dark-navy text-[#fff] flex justify-around items-center h-20 text-[14px] p-5 rounded-lg mb-4">
+        <span className="max-w-[3%] w-full mr-2 px-1">{market_cap_rank}</span>
+        <div className="max-w-[15%] w-full flex px-1 ml-[-12px]">
+          <img src={image} alt="" className="w-[20px] h-auto" />
+          <span className="px-3">{name}</span>
+        </div>
+        <span className="max-w-[8%] w-full px-1 ml-[-12px]">
+          $ {formatNumberWithK(current_price)}
+        </span>
+        <div className=" max-w-[7%] w-full flex ml-[-12px] pr-2">
+          {price_change_percentage_1h_in_currency > 0 && (
+            <img src="/chevron-up.svg" alt="" />
+          )}
+          {price_change_percentage_1h_in_currency < 0 && (
+            <img src="/chevron-down.svg" alt="" />
+          )}
 
-        <span
-          style={{
-            color:
-              price_change_percentage_1h_in_currency > 0
-                ? "#01F1E3"
-                : "#FE2264",
-          }}
-        >
-          {roundToTwoDecimalPlaces(price_change_percentage_1h_in_currency)}%
-        </span>
-      </div>
-      <div className=" max-w-[7%] w-full flex  pr-2">
-        {price_change_percentage_24h_in_currency > 0 && (
-          <img src="/chevron-up.svg" alt="" />
-        )}
-        {price_change_percentage_24h_in_currency < 0 && (
-          <img src="/chevron-down.svg" alt="" />
-        )}
-
-        <span
-          style={{
-            color:
-              price_change_percentage_24h_in_currency > 0
-                ? "#01F1E3"
-                : "#FE2264",
-          }}
-        >
-          {roundToTwoDecimalPlaces(price_change_percentage_24h_in_currency)}%
-        </span>
-      </div>
-      <div className=" max-w-[7%] w-full flex  pr-2">
-        {price_change_percentage_7d_in_currency > 0 && (
-          <img src="/chevron-up.svg" alt="" />
-        )}
-        {price_change_percentage_7d_in_currency < 0 && (
-          <img src="/chevron-down.svg" alt="" />
-        )}
-        <span
-          style={{
-            color:
-              price_change_percentage_7d_in_currency > 0
-                ? "#01F1E3"
-                : "#FE2264",
-          }}
-        >
-          {roundToTwoDecimalPlaces(price_change_percentage_7d_in_currency)}%
-        </span>
-      </div>
-      <div className="max-w-[19%] w-full px-1">
-        <ProgressBar
-          color={{ bright: "#6374C3", background: "#3D4673" }}
-          first={total_volume}
-          second={market_cap}
-        />
-      </div>
-      <div className="max-w-[19%] w-full px-1">
-        <ProgressBar
-          color={{ bright: "#F7931A", background: "#724A20" }}
-          first={circulating_supply}
-          second={total_supply}
-        />
-      </div>
-      <div className="max-w-[14%] w-full">
-        <ResponsiveContainer width="100%" height={60}>
-          <AreaChart
-            data={coinChart}
-            margin={{ top: 10, right: 10, left: 10, bottom: 3 }}
+          <span
+            style={{
+              color:
+                price_change_percentage_1h_in_currency > 0
+                  ? "#01F1E3"
+                  : "#FE2264",
+            }}
           >
-            <defs>
-              <linearGradient id="positiveGradient" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#01F1E3" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#01F1E3" stopOpacity={0} />
-              </linearGradient>
-              <linearGradient id="negativeGradient" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#FE2264" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#FE2264" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <Area
-              dataKey="price"
-              stroke={
+            {roundToTwoDecimalPlaces(price_change_percentage_1h_in_currency)}%
+          </span>
+        </div>
+        <div className=" max-w-[7%] w-full flex  pr-2">
+          {price_change_percentage_24h_in_currency > 0 && (
+            <img src="/chevron-up.svg" alt="" />
+          )}
+          {price_change_percentage_24h_in_currency < 0 && (
+            <img src="/chevron-down.svg" alt="" />
+          )}
+
+          <span
+            style={{
+              color:
+                price_change_percentage_24h_in_currency > 0
+                  ? "#01F1E3"
+                  : "#FE2264",
+            }}
+          >
+            {roundToTwoDecimalPlaces(price_change_percentage_24h_in_currency)}%
+          </span>
+        </div>
+        <div className=" max-w-[7%] w-full flex  pr-2">
+          {price_change_percentage_7d_in_currency > 0 && (
+            <img src="/chevron-up.svg" alt="" />
+          )}
+          {price_change_percentage_7d_in_currency < 0 && (
+            <img src="/chevron-down.svg" alt="" />
+          )}
+          <span
+            style={{
+              color:
                 price_change_percentage_7d_in_currency > 0
                   ? "#01F1E3"
-                  : "#fe2264"
-              }
-              fill={
-                price_change_percentage_7d_in_currency > 0
-                  ? "url(#positiveGradient)"
-                  : "url(#negativeGradient)"
-              }
-              strokeWidth={2}
-              type="monotone"
-            />
-            <YAxis type="number" domain={["dataMin", "dataMax"]} hide={true} />
-          </AreaChart>
-          <Tooltip />
-        </ResponsiveContainer>
+                  : "#FE2264",
+            }}
+          >
+            {roundToTwoDecimalPlaces(price_change_percentage_7d_in_currency)}%
+          </span>
+        </div>
+        <div className="max-w-[19%] w-full px-1">
+          <ProgressBar
+            color={{ bright: "#6374C3", background: "#3D4673" }}
+            first={total_volume}
+            second={market_cap}
+          />
+        </div>
+        <div className="max-w-[19%] w-full px-1">
+          <ProgressBar
+            color={{ bright: "#F7931A", background: "#724A20" }}
+            first={circulating_supply}
+            second={total_supply}
+          />
+        </div>
+        <div className="max-w-[14%] w-full">
+          <ResponsiveContainer width="100%" height={60}>
+            <AreaChart
+              data={coinChart}
+              margin={{ top: 10, right: 10, left: 10, bottom: 3 }}
+            >
+              <defs>
+                <linearGradient
+                  id="positiveGradient"
+                  x1="0"
+                  y1="0"
+                  x2="0"
+                  y2="1"
+                >
+                  <stop offset="5%" stopColor="#01F1E3" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#01F1E3" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient
+                  id="negativeGradient"
+                  x1="0"
+                  y1="0"
+                  x2="0"
+                  y2="1"
+                >
+                  <stop offset="5%" stopColor="#FE2264" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#FE2264" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <Area
+                dataKey="price"
+                stroke={
+                  price_change_percentage_7d_in_currency > 0
+                    ? "#01F1E3"
+                    : "#fe2264"
+                }
+                fill={
+                  price_change_percentage_7d_in_currency > 0
+                    ? "url(#positiveGradient)"
+                    : "url(#negativeGradient)"
+                }
+                strokeWidth={2}
+                type="monotone"
+              />
+              <YAxis
+                type="number"
+                domain={["dataMin", "dataMax"]}
+                hide={true}
+              />
+            </AreaChart>
+            <Tooltip />
+          </ResponsiveContainer>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,9 +1,13 @@
+import { useParams } from "react-router-dom";
+
 import AboutCoin from "../UI/Coin UI/AboutCoin";
 import CoinChart from "../UI/Coin UI/CoinChart";
 import CoinDetails from "../UI/Coin UI/CoinDetails";
 import MarketDetails from "../UI/Coin UI/MarketDetails";
 
 function CoinPage() {
+  const { id } = useParams();
+
   return (
     <div className="px-4 pb-10 pt-2">
       <div className=" flex gap-4 mb-5">
