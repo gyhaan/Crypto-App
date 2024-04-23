96b99c87e4f870f8f7a448582fffa42e7eb02bcd
gyhaan <g.yhaan@alustudent.com>
2024-04-23 21:37:02 +0200

    Finished NavBar

--- src/UI/AppNav.jsx
+++ src/UI/AppNav.jsx
@@ -1,15 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import SelectCurrency from "./SelectCurrency";
 
 function AppNav() {
   return (
     <div className="flex justify-between items-center h-[96px]">
-      <img src="/Logo.svg" alt="" className="w-[171px]" />
+      <Link to="/">
+        <img src="/Logo.svg" alt="" className="w-[171px]" />
+      </Link>
       <nav className="w-[265px]">
-        <ul className="flex  justify-evenly text-[#fff]">
-          <li>Home</li>
-          <li>Home</li>
+        <ul className="flex  justify-evenly text-[#fff] text-[18px]">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </li>
         </ul>
       </nav>
       <div className="flex gap-[16px] w-[544px] justify-end">
--- src/UI/SearchList.jsx
+++ src/UI/SearchList.jsx
@@ -13,13 +13,13 @@ function SearchList() {
   });
   if (isLoading) {
     return (
-      <ul className="list bg-custom-dark-navy w-full absolute h-80 overflow-y-scroll z-10 rounded-lg mt-2">
+      <ul className="list bg-custom-dark-navy w-full absolute h-20 my-auto overflow-y-scroll z-10 rounded-lg mt-2">
         <Loader />
       </ul>
     );
   }
   return (
-    <ul className="list bg-custom-dark-navy w-full absolute h-80 overflow-y-scroll z-10 rounded-lg mt-2">
+    <ul className="list bg-custom-dark-navy w-full absolute max-h-80 overflow-y-scroll z-10 rounded-lg mt-2">
       {searchedCoins
         ?.filter((element) => {
           if (!searchQuery) return element;
